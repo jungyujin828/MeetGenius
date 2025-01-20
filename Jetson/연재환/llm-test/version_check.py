beff41ea434a16100a680bf89c030ab5ea09f833
chanhojeong <chano01794@gmail.com>
2025-01-20 11:20:03 +0900

    git 정리

--- Jetson/연재환/llm-test/version_check.py
+++
@@ -1,9 +0,0 @@
-import torch 
-
-print("torch: ", torch.__version__)
-
-print("GPU: ", torch.cuda.is_available())
-
-print("CUDA VERSION: ", torch.version.cuda if torch.cuda.is_available() else None)
-
-
