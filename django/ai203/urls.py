7af41b730131cd26dbacd33fda89bf89af65cdb0
rmsgnl11 <rmsgnl11@naver.com>
2025-01-25 00:09:15 +0900

    feat(BE): testapp 생성
    FastAPI와 통신테스트를 위해 testapp 생성
    
    #S12P11B203-172

--- django/ai203/urls.py
+++ django/ai203/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('projects/', include('projects.urls')),
     path('meetings/', include('meetings.urls')),
+    path('tests/', include('testapp.urls')),
 ]
---
+++ django/testapp/views.py
@@ -0,0 +1,31 @@
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+# Create your views here.
+
+@csrf_exempt # csrf 일단 비활성화
+def test_view(request):
+    # GET 요청 처리
+    if request.method =="GET":
+        # FastAPI에서 JSON으로 데이터를 전송한 경우, 
+        # Django에서 데이터를 가져오려면 아래와 같이 request.body를 사용해야 합니다:
+        print(request.body)
+        
+        data = {'name' : '김근휘', 'message':'안녕하세요'}
+        print('Get 요청 발생')
+        return JsonResponse(data)
+    # POST 요청 처리
+    elif request.method == "POST":
+        print('***')
+        print(request.POST)
+        print(request.body)
+        print('***')
+        data = json.loads(request.body)
+        print(data,'다시 보내기')
+
+        print('POST 요청 수신')
+        return JsonResponse({'message':'데이터 받았다리', 'received_data' : data}, status=200)
+    else:
+        return JsonResponse({'error':'잘못된 요청'})
