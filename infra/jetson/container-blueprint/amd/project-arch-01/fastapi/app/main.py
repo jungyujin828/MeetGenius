1624d76b94fd0808b4dc053e6b3440371860714d
chanhojeong <chano01794@gmail.com>
2025-01-31 16:31:14 +0900

    refactor(ETC): Git merge
    250131 merge
    
    #S12P11B203-188

--- infra/jetson/container-blueprint/amd/project-arch-01/fastapi/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, HTTPException 
-
-
-# FastAPI 앱 초기화 
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
--- infra/jetson/container-blueprint/amd/project-arch-02/fastapi/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-# import requests
-
-app = FastAPI()
-
-# Root endpoint for health check
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI is running!"}
