181d684ad825358b36813ed471d506edf1acfd58
장인영 <dlsdud0704@naver.com>
2025-02-08 14:47:47 +0900

    chore(BE): 로그인 로직 구현 로그인 인증 방식 변경 (세션 -> 토큰) #S12P11B203-183

--- Web/backend/django/accounts/views.py
+++ Web/backend/django/accounts/views.py
@@ -1,29 +1,39 @@
-from django.http import JsonResponse
+from rest_framework.response import Response  # DRF Response 추가
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from rest_framework import status
 from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login(request):
+    # 이미 로그인된 경우
     if request.user.is_authenticated:
-        return JsonResponse({"message": "이미 로그인되어 있습니다."}, status=200)
-
-    form = AuthenticationForm(request, data={'username': request.data.get('employee_number'), 'password': request.data.get('password')})
-    if form.is_valid():
-        user = form.get_user()
+        return Response({"message": "이미 로그인되어 있습니다."}, status=status.HTTP_200_OK)
+    employee_number= request.data.get('employee_number')
+    password= request.data.get('password')
+    user = authenticate(request, username=employee_number, password=password)
+    if user is not None:
         auth_login(request, user)
+        token, created = Token.objects.get_or_create(user=user)
+        
+        # 사용자 데이터를 serializer로 직렬화하고, 토큰 추가
         user_data = UserSerializer(user).data
-        return JsonResponse(user_data, status=200)
+        user_data['token'] = token.key  # 토큰 추가
+        
+        return Response(user_data, status=status.HTTP_200_OK)
 
-    return JsonResponse({"error": "잘못된 사번 또는 비밀번호입니다."}, status=400)
+    else:
+        print("Form is invalid")
+        return Response({"error": "입력하신 사번 또는 비밀번호가 맞지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
-@permission_classes([AllowAny])  # 로그아웃은 인증 없이 가능해야 함
 def logout(request):
     if request.user.is_authenticated:
-        auth_logout(request)  # 세션 삭제 (로그아웃 처리)
-        return JsonResponse({"message": "성공적으로 로그아웃되었습니다."}, status=200)
-    return JsonResponse({"message": "이미 로그아웃된 상태입니다."}, status=400)
+        auth_logout(request)
+        return Response({"message": "성공적으로 로그아웃되었습니다."}, status=status.HTTP_200_OK)
+    return Response({"error": "로그인 상태가 아닙니다."}, status=status.HTTP_400_BAD_REQUEST)
