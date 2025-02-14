ac8bf02f5a317e6451cf53d14b6964aff12d431f
geunhh <rmsgnl11@naver.com>
2025-02-14 13:06:34 +0900

    feat(BE): 로그인 중인 유저 정보 조회
    로그인 중인 유저 정보 조회
    
    #S12P11B203-287

--- Web/backend/django/accounts/urls.py
+++ Web/backend/django/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login,logout,get_all_departments,get_all_users,get_users_by_department,get_unread_notifications,mark_as_read
+from .views import login,logout,get_all_departments,get_all_users,get_users_by_department,get_unread_notifications,mark_as_read,get_user_info
 
 app_name = "accounts"
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('departments/', get_all_departments, name ='get_all_departments'),
     path('users/', get_all_users, name ='get_all_users'), # 전체 유저 목록
     path('users/department/<int:department_id>/', get_users_by_department, name ='get_users_by_department'), # 부서별 유저 목록
+    path('user_info/',get_user_info,name="get_user_info"),
     path('notifications/unread/', get_unread_notifications, name='get_unread_notifications'),
     path('notifications/mark_as_read/<int:notification_id>/', mark_as_read, name='mark_as_read'),
 ]
