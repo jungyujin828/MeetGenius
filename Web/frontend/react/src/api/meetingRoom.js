928d85476d25b147bbccca98656fb31aee7df272
장인영 <dlsdud0704@naver.com>
2025-02-17 12:51:30 +0900

    feat(FE): 요약된 회의록 조회 요약된 회의록 조회 #S12P11B203-300

--- Web/frontend/react/src/api/meetingRoom.js
+++ Web/frontend/react/src/api/meetingRoom.js
@@ -113,3 +113,14 @@ export const patchMom = async (meetingId, formData) => {
     throw new Error(error.response?.data?.message || "회의상세 정보를 불러오는 데에 실패했습니다.");
   }
 }
+
+export const fetchSummaryMomsByMeetings = async (meetingId) => {
+  try {
+    const response = await axiosInstance.get(
+      `/meetingroom/summarymoms_by_meeting/${meetingId}/`,
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "요약된 회의록을 불러오는 데에 실패했습니다.");
+  }
+};
